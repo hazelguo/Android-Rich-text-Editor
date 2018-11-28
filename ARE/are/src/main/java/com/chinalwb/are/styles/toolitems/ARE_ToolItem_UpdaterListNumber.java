43e473309ac323cd2acb72a7d4da3b1cfcdb7eda
Hazel Guo <hazelguo@asana.com>
2018-11-28 11:23:21 +0000

    Fix bug that ToolItem_UpdaterListNumber should extend from List updater not Default updater

--- ARE/are/src/main/java/com/chinalwb/are/styles/toolitems/ARE_ToolItem_UpdaterListNumber.java
+++ ARE/are/src/main/java/com/chinalwb/are/styles/toolitems/ARE_ToolItem_UpdaterListNumber.java
@@ -2,15 +2,15 @@ package com.chinalwb.are.styles.toolitems;
 
 import java.util.List;
 
-public class ARE_ToolItem_UpdaterListNumber extends ARE_ToolItem_UpdaterDefault {
-    private ARE_ToolItem_ListBullet mTOolItemListBullet;
+public class ARE_ToolItem_UpdaterListNumber extends ARE_ToolItem_UpdaterList {
+    private ARE_ToolItem_ListBullet mToolItemListBullet;
 
     public ARE_ToolItem_UpdaterListNumber(IARE_ToolItem toolItem, int checkedColor, int uncheckedColor) {
         super(toolItem, checkedColor, uncheckedColor);
         List<IARE_ToolItem> toolitems = toolItem.getToolbar().getToolItems();
         for (IARE_ToolItem item : toolitems) {
             if (item instanceof ARE_ToolItem_ListBullet) {
-                mTOolItemListBullet = (ARE_ToolItem_ListBullet) item;
+                mToolItemListBullet = (ARE_ToolItem_ListBullet) item;
                 break;
             }
         }
@@ -20,7 +20,7 @@ public class ARE_ToolItem_UpdaterListNumber extends ARE_ToolItem_UpdaterDefault 
     public void onCheckStatusUpdate(boolean newChecked) {
         super.onCheckStatusUpdate(newChecked);
         if (newChecked) {
-            mTOolItemListBullet.getToolItemUpdater().onCheckStatusUpdate(false);
+            mToolItemListBullet.getToolItemUpdater().onCheckStatusUpdate(false);
         }
     }
 }
